7eb569a2a777d7cfbb8becc3eca30ec780215b56
Waqas Galla <waqaskiffal15@gmail.com>
2019-10-10 16:04:21 +0500

    feat: add react-native

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from 'react-native'
+import { Provider as PaperProvider } from "react-native-paper";
+
+export default function App() {
+  return (
+    // <PaperProvider />
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
